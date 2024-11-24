13050abf20e53c2ace649dc0ac89c4002715f837
tifie <tapingcove@gmail.com>
2024-11-24 14:43:45 +0900

    init

---
+++ backend/routers/setup.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/samber/do"
+)
+
+
+func SetupRouter(router *gin.Engine, injector *do.Injector) {
+  TodoRouter(router, injector)
+}
